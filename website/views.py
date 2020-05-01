b1ca1e88dbb66176d4aad538eaaebbcb805f70ad
raven <32195921+raven404@users.noreply.github.com>
2020-05-01 23:52:26 +0530

    iews for home added

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meeting
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse("Welcome to the planner") 
+
+    return render(request,"website/welcome.html",{"message":"This is the welcome page rendered" ,"number_of_meetings":Meeting.objects.count(),'meeting':Meeting.objects.all})
 
